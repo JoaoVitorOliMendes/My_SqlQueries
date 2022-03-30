5ad8ad1424c9a1f170c26659ea9f29a6ddac0895
JOAO_VITOR_DE_OLIVEIRA_MENDES <72200790@DELTAB31.BARROCA>
2022-03-29 22:20:42 -0300

    first commit

---
+++ College/Ex1.sql
@@ -0,0 +1,18 @@
+--select * from all_tables
+--select * from dependente where nome_dependente like 'M%';
+--select * from empregado where nome_empregado like '%son';
+--select * from empregado where nome_empregado like 'M%';
+--select * from empregado where nome_empregado not like 'Pat%';
+--select * from empregado where salario < 1000;
+--select * from empregado where salario > 2000 and salario < 3000;
+--select * from empregado where salario between 2000 and 3000;
+--select * from empregado where (to_char(admissao, 'MM')) = 05
+--select * from empregado where (to_char(admissao, 'DD/MM/YYYY')) between '10/05/2012' and '30/05/2012'
+--select * from dependente where (to_char(dt_nascimento, 'YYYY')) = '2004';
+--select * from funcionario where nome_funcionario like 'M%c_';
+select * 
+from funcionario
+    join departamento
+    on departamento.cod_departamento = funcionario.cod_setor
+where nome_funcionario like 'M%c_'
+and departamento.nome_departamento = 'Produção';
