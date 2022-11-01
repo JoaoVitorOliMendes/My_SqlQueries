4529a7b594cf5161d0ea60ea858a91df72d581f2
João Vitor de Oliveira Mendes <68301483+JoaoVitorOliMendes@users.noreply.github.com>
2022-10-31 22:04:02 -0300

    Update atv5.sql

--- College/atv5.sql
+++ College/atv5.sql
@@ -64,3 +64,38 @@ end;
 /
 
 call PR_FILIAL_MES('03', 'Filial A');
+
+set serveroutput on;
+
+
+
+select cod_atendimento, to_char(atendimento.data_atendimento, 'dd/MM/yyyy HH24:MI:SS') as data_hora, paciente.nome_paciente from atendimento, funcionario, paciente, tarefa where atendimento.cod_funcionario = funcionario.cod_funcionario and atendimento.cod_paciente = paciente.cod_paciente and funcionario.nome_funcionario = 'Franjinha';
+clear screen;
+create or replace procedure PR_ATENDIMENTO(nomeFunc varchar2)
+as
+cursor cr_atendimento is select cod_atendimento, cod_funcionario, cod_paciente, cod_tarefa, to_char(atendimento.data_atendimento, 'dd/MM/yyyy HH24:MI:SS') as data_hora from atendimento;
+cursor cr_funcionario is select cod_funcionario, nome_funcionario from funcionario;
+cursor cr_paciente is select cod_paciente, nome_paciente from paciente;
+cursor cr_tarefa is select cod_tarefa, nome_tarefa from tarefa;
+begin
+    for w in cr_funcionario loop
+        if w.nome_funcionario = nomeFunc
+            then for x in cr_atendimento loop
+                if x.cod_funcionario = w.cod_funcionario
+                    then for y in cr_paciente loop
+                        if y.cod_paciente = x.cod_paciente
+                            then for z in cr_tarefa loop
+                                if z.cod_tarefa = x.cod_tarefa
+                                    then dbms_output.put_line(x.cod_atendimento||' '||x.data_hora||' '||y.nome_paciente||' '||z.nome_tarefa);
+                                end if;
+                            end loop;
+                        end if;
+                    end loop;
+                end if;
+            end loop;
+        end if;
+    end loop;
+end;
+/
+
+call PR_ATENDIMENTO('Franjinha');
